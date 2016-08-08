b26399f057ed3704d2410d2cf6403a95712a52a0
p <p@ps-MacBook-Air.local>
2016-08-08 16:13:29 -0500

    auth start

--- app/controllers/applicaton.js
+++ app/controllers/applicaton.js
@@ -2,4 +2,10 @@ import Ember from 'ember'
 
 export default Ember.Controller.extend({
   sesssion: Ember.inject.service('session')
+
+  actions: {
+    invalidateSession() {
+      this.get('session').invalidate();
+    }
+  }
 });
